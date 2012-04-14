657b230c01807c453406656c8ec0adb8e19eccd2
Yash Aggarwal <yash0609@gmail.com>
2012-04-13 23:17:10 -0500

    people can add to the list if they are generaous

--- website.rb
+++ website.rb
@@ -7,17 +7,27 @@ $content[1] = ""
 	$content[i] = File.open("content/#{i}.txt", "rb").read.downcase
 end
 
+def another_term(chapter, term)
+	$content[chapter] = $content[chapter] + "\n #{term}"
+	File.open("content/#{chapter}.txt", "w") { |f| f.write($content[chapter]) }
+end
+
 get '/' do
 	erb :index
 end
 
 post '/' do
-	term = params[:term].downcase
+	@term = params[:term].downcase
 	@chapters_having_term = []
 	$content.each_with_index do |text, index|
-		if $content[index].include?(term)
+		if $content[index].include?(@term)
 			@chapters_having_term << index
 		end
 	end
 	erb :result
+end
+
+post '/addterm/?' do
+	another_term(params[:chapter].to_i, params[:term])
+	redirect '/'
 end
