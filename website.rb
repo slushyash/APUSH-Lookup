29aa9ae8ffe2961926a0e87830a2ccc0d8b79a8c
Yash Aggarwal <yash0609@gmail.com>
2012-04-13 22:18:47 -0500

    added config ru, changed stuff

--- website.rb
+++ website.rb
@@ -1,26 +1,23 @@
 require 'sinatra'
-require 'rubygems'
 
 $content = []
-
+$content[0] = ""
+$content[1] = ""
 (2..34).each do |i|
 	$content[i] = File.open("content/#{i}.txt", "rb").read.downcase
 end
-$content[0] = ""
-$content[1] = ""
 
 get '/' do
 	erb :index
 end
 
-get '/result/:term' do
+post '/' do
 	term = params[:term].downcase
-	chapters_having_term = []
+	@chapters_having_term = []
 	$content.each_with_index do |text, index|
-		puts index.to_s
 		if $content[index].include?(term)
-			chapters_having_term << index
+			@chapters_having_term << index
 		end
 	end
-	halt chapters_having_term.to_s
+	erb :result
 end
