c7f29a5cead77d8e2098be0a43fc89f8189d83f4
Yash Aggarwal <yash0609@gmail.com>
2012-04-14 10:33:22 -0500

    added redis functionality to persist user contributed stuff.

--- website.rb
+++ website.rb
@@ -1,15 +1,25 @@
 require 'sinatra'
+require 'redis'
+require 'uri'
 
-$content = []
-$content[0] = ""
-$content[1] = ""
-(2..34).each do |i|
-	$content[i] = File.open("content/#{i}.txt", "rb").read.downcase
+
+configure do
+	# When the application loads, put the text file information into memory
+	# global variables are stupid, but hey, this is a small program
+	$content = []
+	$content[0] = ""
+	$content[1] = ""
+	(2..34).each do |i|
+		$content[i] = File.open("content/#{i}.txt", "rb").read.downcase
+	end
+
+	# Configure the redis instance for user-contributed lookups
+	uri = URI.parse('redis://redistogo:dcf879b4a42316b82526f8ae7cdbdac6@drum.redistogo.com:9168')
+	REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 end
 
 def another_term(chapter, term)
-	$content[chapter] = $content[chapter] + "\n #{term}"
-	#File.open("content/#{chapter}.txt", "w") { |f| f.write($content[chapter]) }
+	REDIS.rpush term, chapter
 end
 
 get '/' do
@@ -18,12 +28,16 @@ end
 
 post '/' do
 	@term = params[:term].downcase
-	@chapters_having_term = []
+	chapters_from_notes = []
 	$content.each_with_index do |text, index|
 		if $content[index].include?(@term)
-			@chapters_having_term << index
+			chapters_from_notes << index
 		end
 	end
+	user_contributed_results = REDIS.lrange @term, 0, -1
+	user_contributed_results = user_contributed_results.collect { |chapter| chapter.to_i }
+	@chapters_having_term = (user_contributed_results + chapters_from_notes).sort!.uniq
+	puts user_contributed_results
 	erb :result
 end
 
